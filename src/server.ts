976268e9ec146199dddc81698a6b52f59358ad22
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-06-02 22:12:51 -0300

    GCP

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import { logEvents } from './middlewares/logEvents' //imports logEvents function
-import { startServer } from './services/databaseConnection'
-import express from 'express' //express
-import cookieParser from 'cookie-parser' //For cookies
-import rootRoutes from './routes/root'
-import apiRoutes from './routes/api/api'
-import { join } from 'path'
-export const app = express() //express
+import { logEvents } from "./middlewares/logEvents"; //imports logEvents function
+import { startServer } from "./services/databaseConnection";
+import express from "express"; //express
+import cookieParser from "cookie-parser"; //For cookies
+import rootRoutes from "./routes/root";
+import apiRoutes from "./routes/api/api";
+import { join } from "path";
+export const app = express(); //express
 
-startServer()
+startServer();
 
-app.use(express.urlencoded({ extended: false })) //For post requests from forms
-app.use(express.static(join(__dirname, 'public')))
-app.use(logEvents.logRequest) //Logging requests on logs folder
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: false })); //For post requests from forms
+app.use(express.static(join(__dirname, "public")));
+app.use(logEvents.logRequest); //Logging requests on logs folder
+app.use(cookieParser());
 
-app.use('/', rootRoutes) //Routes for pages
-app.use('/api', apiRoutes) //Routes for api
+app.use("/", rootRoutes); //Routes for pages
+app.use("/api", apiRoutes); //Routes for api
--- src/services/databaseConnection.ts
+++ src/services/databaseConnection.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { app } from '../server'
+import { app } from "../server";
 import { logEvents } from "../middlewares/logEvents";
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient();
 
-export const startServer =  async () => {
-    try {
-        await prisma.$connect()
-        app.listen(4040, () => {
-            console.log('Server running...')
-            logEvents.databaseConnect()
-            logEvents.serverStatus()
-        })
-    } catch (error) {
-        logEvents.databaseConnect(error)
-        logEvents.serverStatus(error)
-    }
-}
+export const startServer = async () => {
+  try {
+    await prisma.$connect();
+    app.listen(3000, () => {
+      console.log("Server running...");
+      logEvents.databaseConnect();
+      logEvents.serverStatus();
+    });
+  } catch (error) {
+    logEvents.databaseConnect(error);
+    logEvents.serverStatus(error);
+  }
+};
